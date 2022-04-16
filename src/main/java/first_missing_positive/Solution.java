4539b34fc11c7c68eec0bedb27055ac49c321bbd
Evgeny <ef7@mail.ru>
2022-04-17 00:59:07 +0300

    first-missing-positive: wip

---
+++ src/main/java/first_missing_positive/Solution.java
@@ -0,0 +1,16 @@
+package first_missing_positive;
+
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        // find 1
+        // 1 is absent - return 1
+        // find next gap after 1
+        // return min in a gap
+        // if no gap is present - return max value + 1
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+
+        }
+        return 0;
+    }
+}
