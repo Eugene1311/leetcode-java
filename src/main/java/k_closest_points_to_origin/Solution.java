bf2d14357f355b9adf4f4194bbba5580c4b9a61b
Evgeny <ef7@mail.ru>
2022-05-06 17:03:35 +0300

    add k_closest_points_to_origin

---
+++ src/main/java/k_closest_points_to_origin/Solution.java
@@ -0,0 +1,27 @@
+package k_closest_points_to_origin;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        List<int[]> sortedPoints = Arrays.stream(points)
+                .sorted((p1, p2) ->
+                        (int) (Math.pow(p1[0], 2) + Math.pow(p1[1], 2) - Math.pow(p2[0], 2) - Math.pow(p2[1], 2)))
+                .collect(Collectors.toList());
+
+        int[][] result = new int[k][2];
+        IntStream.range(0, k)
+                .mapToObj(sortedPoints::get)
+                .collect(Collectors.toList())
+                .toArray(result);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(Arrays.deepToString(solution.kClosest(new int[][]{{1, 1}, {1, 3}, {-2, 2}}, 2)));
+    }
+}
