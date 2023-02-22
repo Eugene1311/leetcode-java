7fa39f415d1c8b33b0d6a25562ae06ab9d288330
Evgeny <ef7@mail.ru>
2023-02-22 21:19:29 +0300

    add divide_two_integers

---
+++ src/main/java/divide_two_integers/Solution.java
@@ -0,0 +1,38 @@
+package divide_two_integers;
+
+public class Solution {
+    public int divide(int dividend, int divisor) {
+        boolean isNegative = (long) dividend * (long) divisor < 0;
+        long longDividend = Math.abs((long) dividend);
+        long longDivisor = Math.abs((long) divisor);
+        long quotient = 0;
+        long subtrahend = 0;
+
+        for (int i = 31; i >= 0; i--) {
+            subtrahend = (subtrahend << 1) + ((longDividend >> i) & 1);
+            if (subtrahend < longDivisor) {
+                quotient = quotient << 1;
+            } else {
+                quotient = (quotient << 1) + 1;
+                subtrahend -= longDivisor;
+            }
+        }
+
+        if (isNegative) {
+            quotient *= -1;
+        }
+        long result = quotient;
+        result = Math.min(Integer.MAX_VALUE, result);
+        result = Math.max(Integer.MIN_VALUE, result);
+        return (int) result;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.divide(10, 3));
+        System.out.println(solution.divide(7, -3));
+        System.out.println(solution.divide(-2147483648, -1));
+        System.out.println(solution.divide(-1010369383, -2147483648));
+        System.out.println(solution.divide(-2147483648, -1109186033));
+    }
+}
