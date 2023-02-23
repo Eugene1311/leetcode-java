399fb4c50aa3bf267385eff527847b95f7300a9e
evgenyfedoseev <ef7@mail.ru>
2023-02-23 15:21:39 +0300

    remove long values in divide_two_integers

--- src/main/java/divide_two_integers/Solution.java
+++ src/main/java/divide_two_integers/Solution.java
@@ -2,29 +2,30 @@ package divide_two_integers;
 
 public class Solution {
     public int divide(int dividend, int divisor) {
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+
         boolean isNegative = (dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0);
-        long longDividend = Math.abs((long) dividend);
-        long longDivisor = Math.abs((long) divisor);
-        long quotient = 0;
-        long subtrahend = 0;
+        int absDividend = Math.abs(dividend);
+        int absDivisor = Math.abs(divisor);
+        int quotient = 0;
+        int subtrahend = 0;
 
         for (int i = 31; i >= 0; i--) {
-            subtrahend = (subtrahend << 1) + ((longDividend >> i) & 1);
-            if (subtrahend < longDivisor) {
+            subtrahend = (subtrahend << 1) + ((absDividend >>> i) & 1);
+            if (subtrahend - absDivisor < 0) {
                 quotient = quotient << 1;
             } else {
                 quotient = (quotient << 1) + 1;
-                subtrahend -= longDivisor;
+                subtrahend -= absDivisor;
             }
         }
 
         if (isNegative) {
             quotient = ~quotient + 1;
         }
-        long result = quotient;
-        result = Math.min(Integer.MAX_VALUE, result);
-        result = Math.max(Integer.MIN_VALUE, result);
-        return (int) result;
+        return quotient;
     }
 
     public static void main(String[] args) {
@@ -32,6 +33,7 @@ public class Solution {
         System.out.println(solution.divide(10, 3));
         System.out.println(solution.divide(7, -3));
         System.out.println(solution.divide(-2147483648, -1));
+        System.out.println(solution.divide(-2147483648, 1));
         System.out.println(solution.divide(-1010369383, -2147483648));
         System.out.println(solution.divide(-2147483648, -1109186033));
     }
