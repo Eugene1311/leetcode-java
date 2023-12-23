877d8d42c9ad91c608ddc3941a480a77ee39bfd0
evgenyfedoseev <ef7@mail.ru>
2023-12-23 19:42:18 +0300

    minor fixes in guess_number_higher_or_lower

--- src/main/java/guess_number_higher_or_lower/Solution.java
+++ src/main/java/guess_number_higher_or_lower/Solution.java
@@ -2,25 +2,24 @@ package guess_number_higher_or_lower;
 
 class GuessGame {
     protected int guess(int picked) {
-        int num = 2;
+        int num = 2147_483_647;
         return Integer.compare(num, picked);
     }
 }
 
 public class Solution extends GuessGame {
     public int guessNumber(int n) {
-        int result = n / 2;
-        int x;
-        int min = 0;
+        int min = 1;
         int max = n;
+        int result = (max - min) / 2;
+        int x;
         while ((x = guess(result)) != 0) {
             if (x == 1) {
                 min = result;
-                int diff = (max - result) / 2 != 0 ? (max - result) / 2 : 1;
-                result += diff;
+                result += (max- min + 1) / 2;
             } else if (x == -1) {
                 max = result;
-                result -= (result - min) / 2;
+                result -= (max - min + 1) / 2;
             }
         }
         return result;
@@ -28,6 +27,6 @@ public class Solution extends GuessGame {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.guessNumber(2));
+        System.out.println(solution.guessNumber(2147483647));
     }
 }
