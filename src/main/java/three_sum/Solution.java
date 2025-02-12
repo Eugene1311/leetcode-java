07fced9367bb2c9470873bae6a2ba3f8026683f3
Evgeny <ef7@mail.ru>
2025-02-12 23:55:47 +0300

    add three_sum

---
+++ src/main/java/three_sum/Solution.java
@@ -0,0 +1,33 @@
+package three_sum;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> passedTriples = new HashSet<>();
+
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            int startPosition = i + 1;
+            int endPosition = nums.length - 1;
+            int target = -nums[i];
+            while (startPosition < endPosition) {
+                int sum = nums[startPosition] + nums[endPosition];
+                if (!passedTriples.contains(List.of(nums[i], nums[startPosition], nums[endPosition])) && sum == target) {
+                    passedTriples.add(List.of(nums[i], nums[startPosition], nums[endPosition]));
+                }
+                if (sum > target) {
+                    endPosition--;
+                } else {
+                    startPosition++;
+                }
+            }
+        }
+
+        return passedTriples.stream()
+                .toList();
+    }
+}
